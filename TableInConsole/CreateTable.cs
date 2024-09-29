46d6a4053d80d20f8fee391ec1df9b546b0e3d16
KirSol <37743428+Xam1l1on@users.noreply.github.com>
2024-09-29 18:40:43 +0300

    Add project files.

---
+++ TableInConsole/CreateTable.cs
@@ -0,0 +1,87 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TableInConsole
+{
+    public static class CreateTable
+    {
+        static char symbolPlus = '+';
+        static string horizontalBorder = "";
+        static int widthString;
+        public static void WriteTable(int n, string s)
+        {
+            Console.Clear();
+            int widthString = 2 * n + s.Length;
+            horizontalBorder = new string (symbolPlus, widthString);
+            for (int i = 0; i < 3; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine($"{symbolPlus}{horizontalBorder}{symbolPlus}");
+                        PrintFirstLine(n, s, widthString);
+                        break;
+                    case 1:
+                        Console.WriteLine($"{symbolPlus}{horizontalBorder}{symbolPlus}");
+                        PrintSecondLine(n,widthString);
+                        break;
+                    case 2:
+                        Console.WriteLine($"{symbolPlus}{horizontalBorder}{symbolPlus}");
+                        PrintThirdLine(widthString);
+                        Console.WriteLine($"{symbolPlus}{horizontalBorder}{symbolPlus}");
+                        break;
+                }
+            }
+        }
+        private static void PrintFirstLine(int n, string s, int totalWidth)
+        {
+            for (int i = 0; i <= 2 * (n - 1); i++)
+            {
+                if (i == (n - 1))
+                {
+                    Console.WriteLine($"+" + new string(' ', n) + s + new string(' ', n) + "+");
+                }
+                else
+                {
+                    Console.WriteLine($"+" + new string(' ', n) + new string(' ', s.Length) + new string(' ', n) + "+");
+                }
+            }
+        }
+        private static void PrintSecondLine(int n,int totalWidth)
+        {
+            string line ="";
+            int i = 1;
+            do
+            {
+                for (int j = 0; j < totalWidth; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        line += (j % 2 == 0) ? "+" : " ";
+                    }
+                    else
+                    {
+                        line += (j % 2 == 0) ? " " : "+";
+                    }
+                }
+                Console.WriteLine($"{symbolPlus}{line}{symbolPlus}");
+                i++;
+                line = string.Empty;
+            }
+            while (i < 2 * n);
+        }
+        private static void PrintThirdLine(int totalWidth)
+        {
+            for (int i = 0; i < totalWidth; i++)
+            {
+                string line = "";
+                for (int j = 0; j < totalWidth; j++)
+                {
+                    line += (i == j || j == totalWidth - i - 1) ? "+" : " ";
+                }
+                Console.WriteLine($"{symbolPlus}{line}{symbolPlus}");
+            }
+        }
+    }
+}
---
+++ TableInConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TableInConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber;
+            string userString;
+
+
+            //The user enters any number.
+            Console.WriteLine("Введите число от 1 до 6");
+            if (!int.TryParse(Console.ReadLine(), out userNumber))
+            {
+                Console.WriteLine($"Это не число. Пока");
+                return;
+            }
+            else if (userNumber < 1 && userNumber > 6)
+            {
+                Console.WriteLine("Число должно быть больше 1 именьше 6");
+                return;
+            }
+            //The user enters any word.
+            Console.WriteLine("Введите слово");
+            userString = Console.ReadLine();
+            if(string.IsNullOrEmpty(userString))
+            {
+                Console.WriteLine("Напиши хоть что-то");
+                return;
+            }
+            //Make the table
+            CreateTable.WriteTable(userNumber, userString);
+        }
+
+    }
+}
