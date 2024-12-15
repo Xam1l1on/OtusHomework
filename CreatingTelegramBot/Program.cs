1700adcf715fa1db35b39680cbff6a551c47a52b
K.Solomatin <k.solomatin@agrohold.ru>
2024-12-15 17:30:46 +0300

    added the setting of the telegram bot/ prev commit add in gitignore file appsettings.json

--- CreatingTelegramBot/Program.cs
+++ CreatingTelegramBot/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace CreatingTelegramBot
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+namespace CreatingTelegramBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static TelegramBotClient BotClient { get; set; }
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            SettingTelegramBot();
+            var me = await BotClient.GetMe();
+            Console.WriteLine(me);
+        }
+        private static void SettingTelegramBot()
+        {
+            //BotSettings botSettings = new BotSettings();
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            BotSettings botSettings = config.GetSection("AppSettings").Get<BotSettings>();
+
+            botSettings.TokenBot = config["AppSettings:AppToken"];
+
+            string? token = botSettings.TokenBot;
+            using var cts = new CancellationTokenSource();
+
+            if (!String.IsNullOrEmpty(token))
+            {
+                BotClient = new TelegramBotClient(token, cancellationToken: cts.Token);
+            }
         }
     }
 }
