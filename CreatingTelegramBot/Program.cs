cd1107618e600d87133f8b576c733b73b6e46f6e
KirSol <37743428+Xam1l1on@users.noreply.github.com>
2025-01-12 21:31:37 +0300

    added delegates and message handler events

--- CreatingTelegramBot/Program.cs
+++ CreatingTelegramBot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
 namespace CreatingTelegramBot
 {
     internal class Program
@@ -8,19 +9,27 @@ namespace CreatingTelegramBot
         private static TelegramBotClient BotClient { get; set; }
         static async Task Main(string[] args)
         {
-
             SettingTelegramBot();
-            var me = await BotClient.GetMe();
-            Console.WriteLine(me);
+            var whoIsBot = await BotClient.GetMe();
+            Console.WriteLine($" Bot is {whoIsBot.FirstName}, {whoIsBot.Username}, {whoIsBot.LastName}");
+            var receiver = new ReceiverOptions
+            {
+                AllowedUpdates = [UpdateType.Message],
+                DropPendingUpdates = true
+            };
+            var handler = new UpdateHandler();
+            handler.OnHandleUpdateStarted += (message) => Console.WriteLine("Началась обработка сообщения '{0}'",message);
+            handler.OnHandleUpdateCompleted += (message) => Console.WriteLine("Закончилась обработка сообщения '{0}'", message);
+            BotClient.StartReceiving(updateHandler: handler, receiverOptions: receiver);
+            await Task.Delay(-1);
         }
         private static void SettingTelegramBot()
         {
-            //BotSettings botSettings = new BotSettings();
             IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
-            BotSettings botSettings = config.GetSection("AppSettings").Get<BotSettings>();
+            BotSettings botSettings = config.GetSection("BotConfig").Get<BotSettings>();
 
-            botSettings.TokenBot = config["AppSettings:AppToken"];
+            botSettings.TokenBot = config["BotConfig:BotToken"];
 
             string? token = botSettings.TokenBot;
             using var cts = new CancellationTokenSource();
---
+++ CreatingTelegramBot/UpdateHandler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace CreatingTelegramBot
+{
+    internal class UpdateHandler : IUpdateHandler
+    {
+        internal delegate void HandleUpdate(string message);
+        internal event HandleUpdate? OnHandleUpdateStarted;
+        internal event HandleUpdate? OnHandleUpdateCompleted;
+        
+        public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var message = update.Message?.Text;
+            OnHandleUpdateStarted?.Invoke(message);
+            if (update.Type == UpdateType.Message && update.Message?.Type == MessageType.Text)
+            {
+                await BotOnMessageReceived(botClient, update.Message);
+            }
+            OnHandleUpdateCompleted?.Invoke(message);
+        }
+        
+        private async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
+        {
+            await botClient.SendMessage(chatId: message.Chat.Id, text: $"Сообщение успешно принято");
+        }
+    }
+}
