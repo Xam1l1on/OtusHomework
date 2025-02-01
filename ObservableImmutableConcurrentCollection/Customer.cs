b4c5bd265fbe5aaa947b21138e7c90bc9f7523f2
KirSol <37743428+Xam1l1on@users.noreply.github.com>
2025-02-01 17:40:42 +0300

    Add homework "ObservableImmutableConcurrentCollection"

---
+++ ObservableImmutableConcurrentCollection/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableImmutableConcurrentCollection
+{
+    public class Customer
+    {
+        public void OnItemChanged()
+        {
+
+        }
+    }
+}
---
+++ ObservableImmutableConcurrentCollection/Shop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableImmutableConcurrentCollection
+{
+    public class Shop
+    {
+        List<Item> items;
+        public void Add(Item item)
+        {
+            //Observable collection, при добавлении item обновляется информация в Customer
+        }
+        public void Remove(Item item)
+        {
+
+        }
+    }
+}
